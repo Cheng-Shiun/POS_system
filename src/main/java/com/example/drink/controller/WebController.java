95165f85105e1d86492f4e394de8de155b0459e5
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-01-05 23:37:22 +0800

    InterfaceWithConnection v1.1
    fixed Web Controller

---
+++ src/main/java/com/example/drink/controller/WebController.java
@@ -0,0 +1,80 @@
+package com.example.drink.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class WebController
+{
+
+    @GetMapping("/Homepage")
+    public String getHomePage()
+    {
+        return "HomePage";
+    }
+    @GetMapping("/menu")
+    public String getMenu()
+    {
+        return "Menu";
+
+    }
+    @GetMapping("/aboutus")
+    public String getAboutUs()
+    {
+        return"AboutUs";
+
+    }
+    @GetMapping("/latestnews")
+    public String getLatestNews()
+    {
+        return"LatestNews";
+
+    }
+    @GetMapping("/contactus")
+    public String getContactUs()
+    {
+        return"ContactUs";
+    }
+    @GetMapping("/coordinates")
+    public String getCoordinates()
+    {
+        return"Coordinates";
+    }
+    @GetMapping("/franchise")
+    public String getFranchise()
+    {
+        return"Franchise";
+    }
+    @GetMapping("/member")
+    public String getMember()
+    {
+        return"Member";
+    }
+    @GetMapping("/qna")
+    public String getQnA()
+    {
+        return"Main";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
