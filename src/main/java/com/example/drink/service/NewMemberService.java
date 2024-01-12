fdae8c30139350af6ed213047156ec8fc8917fbd
teacher <miscoder001@gmail.com>
2024-01-12 13:54:53 +0800

    20240112 1354 update

--- src/main/java/com/example/drink/service/NewMemberService.java
+++ src/main/java/com/example/drink/service/NewMemberService.java
@@ -1,4 +1,38 @@
 package com.example.drink.service;
 
+import com.example.drink.Dao.NewMemberRepository;
+import com.example.drink.model.MemberParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewMemberService {
+    @Autowired
+    NewMemberRepository newMemberRepository;
+
+    public int newMember(MemberParam memberParam) {
+        if (memberParam.getName().contains("select") || memberParam.getName().contains("delate")) {
+            return 3;
+        }
+
+        if ( checkMember(memberParam.getName())) {
+            return 2;
+        }
+
+        int count = newMemberRepository.newMember(memberParam);
+        if (count > 0) {
+            return 1;
+        } else {
+            return count;
+        }
+    }
+
+    public boolean checkMember (String memberName) {
+        long count = newMemberRepository.checkMember(memberName);
+        if (count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
